281cb5ca6560ad1d09d6853533db68677e42be8c
Dr-P-Chitralingappa <88087923+Dr-P-Chitralingappa@users.noreply.github.com>
2025-12-01 11:49:30 +0530

    Add subtitle 'Associate Professor' to Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
   return (
     <nav style={{ padding: "20px", background: "#222", color: "white" }}>
       <h2>Dr. P. Chitralingappa</h2>
+      <h3>Associate Professor</h3>
     </nav>
   );
 }
